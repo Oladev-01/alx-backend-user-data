8fc7859fd3ded3b84584b6d032aebdc28fbbbc6e
olalekan <lekanmojibola@gmail.com>
2024-09-13 18:59:07 +0100

    hashing pii

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,5 @@ import re
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str):  # noqa
     """obsfucating"""
-    if fields is None or redaction is None:
-        return message
     pattern = f"({'|'.join(fields)})=[^{separator}]+"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
