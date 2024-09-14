f217c15f15a442a88a16a01462f11bec53f0fb37
olalekan <lekanmojibola@gmail.com>
2024-09-14 08:06:58 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,7 +37,7 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    formatter = RedactingFormatter(["email", "ssn", "password"])  # noqa
+    formatter = RedactingFormatter(["name", "phone", "email", "ssn", "ip"])  # noqa
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
