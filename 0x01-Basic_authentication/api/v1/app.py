f664e6ec6dbe5655253bfb6b79975c6db410ec83
olalekan <lekanmojibola@gmail.com>
2024-09-17 23:03:55 +0100

    This is a python code

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,7 +16,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 a = Auth()
 auth = getenv("AUTH_TYPE")
-
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 @app.before_request
 def check_auth():
