cd471d16ea9434f3dbf71afaa64a234b9d6af367
olalekan <lekanmojibola@gmail.com>
2024-09-19 09:36:50 +0100

    This is a python code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 """basic auth"""
+from werkzeug.datastructures import auth
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """basic auth"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
+        """extract base64 from header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[len("Basic "):]
