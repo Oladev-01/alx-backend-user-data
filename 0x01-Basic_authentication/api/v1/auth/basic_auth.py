8c229da0b115b5a618b150f21277541ffa492f5d
olalekan <lekanmojibola@gmail.com>
2024-09-19 10:54:42 +0100

    This is a python code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """basic auth"""
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -15,3 +17,13 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # noqa
+        """decode base64 from header"""
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')  # noqa
+        except binascii.Error:
+            return None
