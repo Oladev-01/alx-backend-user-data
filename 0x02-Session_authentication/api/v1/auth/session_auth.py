881375c0a65e56124fe6744202230bcdcbad69fe
olalekan <lekanmojibola@gmail.com>
2024-09-20 09:28:25 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,11 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns user instance based on cookie id"""
+        get_session_id = self.session_cookie(request)
+        if get_session_id is None:
+            return None
+        return self.user_id_for_session_id(get_session_id)
+    
