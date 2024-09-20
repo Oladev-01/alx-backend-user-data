df17bfff96d708bdd54af9ea7fdb052965e5047a
olalekan <lekanmojibola@gmail.com>
2024-09-20 08:41:36 +0100

    This is a python code

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,10 +38,8 @@ def check_auth():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.session_cookie(request) is None:
-        abort(401)
-
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+        auth.session_cookie(request) is None):
         abort(401)
 
     get_user = auth.current_user(request)
