56d0c6905193c49f52dbc62fd7d5494c306b44cf
olalekan <lekanmojibola@gmail.com>
2024-09-25 14:14:42 +0100

    db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""db model"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hash_pwd: str) -> User:
+        """add to database"""
+        new_user = User(email=email, hashed_password=hash_pwd)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
