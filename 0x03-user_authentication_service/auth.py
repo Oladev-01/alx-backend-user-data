37dfb83adeb3f061dce0ae43eeebc4573c2a0de2
olalekan <lekanmojibola@gmail.com>
2024-09-26 23:35:18 +0100

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,7 +26,6 @@ def _generate_uuid() -> str:
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         self._db = DB()
 
@@ -48,3 +47,14 @@ class Auth:
                                   user.hashed_password)
         except (InvalidRequestError, NoResultFound):
             return False
+
+    def create_session(self, email: str) -> str:
+        """returns session id"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+
+        except (NoResultFound, InvalidRequestError):
+            return None
