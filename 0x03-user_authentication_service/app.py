1b1a75bc400d4d517ccacf3a2991601cd123a60b
olalekan <lekanmojibola@gmail.com>
2024-09-27 18:14:44 +0100

    This is a python code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -109,5 +109,18 @@ def reset_password() -> str:
     return jsonify(msg), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_pwd():
+    """updates the password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
