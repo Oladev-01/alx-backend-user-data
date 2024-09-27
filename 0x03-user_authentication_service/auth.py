7d4c04b7c9fe14ac0af8ca98e3701c3b971cb6a0
olalekan <lekanmojibola@gmail.com>
2024-09-27 08:38:31 +0100

    This is a python code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,10 +60,16 @@ class Auth:
         except (NoResultFound, InvalidRequestError):
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
-        """returns the user by session id"""
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """It takes a single session_id string argument
+        Returns a string or None
+        """
+        if session_id is None:
+            return None
+
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user
-        except (NoResultFound, InvalidRequestError):
+        except NoResultFound:
             return None
+
+        return user
