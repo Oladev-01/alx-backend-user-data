1e8a6a80b99a63e8e770383df7047aecc7faf1ae
olalekan <lekanmojibola@gmail.com>
2024-09-27 13:13:37 +0100

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,14 @@ class Auth:
             return None
         except (InvalidRequestError, NoResultFound, ValueError):
             return None
+
+
+    def get_reset_password_token(self, email: str) -> str:
+        """generate a new token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            new_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=new_token)
+            return new_token
+        except (InvalidRequestError, NoResultFound):
+            raise ValueError
