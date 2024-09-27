6d5f29bd03d284061564f9bcfcdc37e115228778
olalekan <lekanmojibola@gmail.com>
2024-09-27 13:21:02 +0100

    destroy session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -99,7 +99,7 @@ def get_reset_password_token() -> str:
         abort(403)
     try:
         token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": token})
+        return jsonify({"email": email, "reset_token": token}), 200
     except ValueError:
         abort(403)
 
