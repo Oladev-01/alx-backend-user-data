6b429339492938b0c757c587190069da4f0d5df6
olalekan <lekanmojibola@gmail.com>
2024-09-27 08:37:07 +0100

    This is a python code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,7 +7,7 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 import bcrypt
-from typing import Any
+from typing import Union
 from user import User
 
 
@@ -60,7 +60,7 @@ class Auth:
         except (NoResultFound, InvalidRequestError):
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """returns the user by session id"""
         try:
             user = self._db.find_user_by(session_id=session_id)
