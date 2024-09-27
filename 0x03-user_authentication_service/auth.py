42f5f1de297ca185ecd6ef2cb565c71687eade77
olalekan <lekanmojibola@gmail.com>
2024-09-27 13:16:01 +0100

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,15 +81,13 @@ class Auth:
         except (InvalidRequestError, NoResultFound, ValueError):
             return None
 
+
     def get_reset_password_token(self, email: str) -> str:
-        """Generates a reset password token if user exists"""
+        """generate a new token"""
         try:
             user = self._db.find_user_by(email=email)
-        except NoResultFound:
+            new_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=new_token)
+            return new_token
+        except (InvalidRequestError, NoResultFound):
             raise ValueError
-
-        reset_token = _generate_uuid()
-
-        self._db.update_user(user.id, reset_token=reset_token)
-
-        return reset_token
